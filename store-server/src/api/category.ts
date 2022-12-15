024c112428cef85b41ce1a92eb431472ff3c4b63
วรรณศักดิ์ หนูปลอด <you007543@gmail.com>
2022-12-15 14:52:01 +0700

    mock ข้อมูลใน category

---
+++ store-server/src/api/category.ts
@@ -0,0 +1,9 @@
+import Router from 'koa-router'
+const router = new Router()
+router.get('/', async ctx =>{
+    ctx.body= [
+        {id:1, title:'Programming'},
+        {id:2, title:'Life Style'}
+    ]})
+    
+export default router
