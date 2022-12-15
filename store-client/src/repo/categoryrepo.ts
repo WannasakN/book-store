65332ece72dfcd9ff82925a55182ac30b98cef1f
วรรณศักดิ์ หนูปลอด <you007543@gmail.com>
2022-12-15 18:12:57 +0700

    สร้างcategory repo โดยการmockข้อมูลในcode

---
+++ store-client/src/repo/categoryrepo.ts
@@ -0,0 +1,12 @@
+import category from "../model/category";
+import { IRepository } from "./IRepository";
+
+
+export class CategoryRepository implements IRepository<category> {
+  async getAll(): Promise<category[] | null>{
+    return [
+        {id:1, title:'Programming'},
+        {id:2, title:'Life Style'}
+    ]
+  }
+}  
