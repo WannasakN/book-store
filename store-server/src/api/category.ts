397d97d6937cebd16935fe031344fda55c630c63
วรรณศักดิ์ หนูปลอด <you007543@gmail.com>
2022-12-16 23:26:56 +0700

    เพิ่มฟังชันให้categoryครบทั้ง5method

--- store-server/src/api/category.ts
+++ store-server/src/api/category.ts
@@ -1,11 +1,13 @@
 import Router from 'koa-router'
 import db from '../db'
 const router = new Router()
-const findById = (id: number) => db('category').select('*').where({id})
+
+const makeQuery = () => db('category').select('*')
+const findById = (id: number) => makeQuery().where({id})
 
 router
   .get('/', async (ctx, next) => {            
-    ctx.body = await db('category').select('*').orderBy('id')
+    ctx.body = await makeQuery().orderBy('id')
   })
   .get('/:id', async (ctx, next) => {
     const id = parseInt(ctx.params.id)
@@ -15,5 +17,31 @@ router
       return
     }
     ctx.body = category
-})  
+  })
+  .post('/', async (ctx, next) => {    
+    const result = await db('category').insert(ctx.request.body)
+    if(result.length <= 0){
+      ctx.response.status = 400
+      return
+    }
+    const id = result[0]
+    ctx.body = await findById(id).first()
+  })
+  .put('/:id', async (ctx, next) => {
+    const id = parseInt(ctx.params.id)
+    delete ctx.request.body.id
+    const rowUpdated = await findById(id).update(ctx.request.body)
+    if(rowUpdated == 0){
+      ctx.response.status = 404
+      return
+    }
+    ctx.body = await findById(id).first()
+  })
+  .del('/:id', async (ctx, next) => {
+    const id = parseInt(ctx.params.id)
+    const rowUpdated = await findById(id).del()
+    ctx.body = {statusCode: rowUpdated > 0 ? 1 : 0}
+  })
+
+
 export default router
