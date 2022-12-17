78cc1d0a053a292d51c3279053932025396a0b77
วรรณศักดิ์ หนูปลอด <you007543@gmail.com>
2022-12-17 15:58:47 +0700

    เพิ่ม5method

--- store-client/src/repo/categoryrepo.ts
+++ store-client/src/repo/categoryrepo.ts
@@ -1,13 +1,39 @@
-import category from "../model/category";
+import Category from "../model/category";
 import { IRepository } from "./IRepository";
+import config from "../config";
+import axios from 'axios';
 
+export class CategoryRepository implements IRepository<Category> {
+  urlPrefix = config.remoteRepositoryUrlPrefix
 
-export class CategoryRepository implements IRepository<category> {
-  async getAll(): Promise<category[] | null>{
-    return [
-        {id:1, title:'Programming'},
-        {id:2, title:'Life Style'}
-    ]
+  async getAll(): Promise<Category[] | null>{
+    const result = await axios.get<Category[]>(`${this.urlPrefix}/category`)
+
+    return result.data
   }
-}  
+
+  async get(id: number|string): Promise<Category | null>{
+    const result = await axios.get<Category>(`${this.urlPrefix}/category/${id}`)
+    
+    return result.data
+  }
+
+  async create(entity: Partial<Category>): Promise<void>{
+    const result = await axios.post<Category>(`${this.urlPrefix}/category`, entity)
+    console.log(result.data)
+  }
+
+  async update(entity: Partial<Category>): Promise<void>{
+    const result = await axios.put<Category>(
+      `${this.urlPrefix}/category/${entity.id}`, 
+      entity
+    )
+    console.log(result.data)
+  }
+
+  async delete(id: number|string): Promise<void>{
+    const result = await axios.delete<Category>(`${this.urlPrefix}/category/${id}`)
+    console.log(result.data)
+  }
+}
    
