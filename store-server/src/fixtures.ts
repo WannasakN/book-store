b2681419b3c5ecb4e8ee019a7da952a0c613f6b9
วรรณศักดิ์ หนูปลอด <you007543@gmail.com>
2022-12-17 16:18:33 +0700

    เพิ่ม fixtures book

--- store-server/src/fixtures.ts
+++ store-server/src/fixtures.ts
@@ -10,6 +10,11 @@ async function loadFixtures(clearData = false){
     { id: 1, title: 'Fantasy' },
     { id: 2, title: 'Computer' },
   ])
+  await db.batchInsert('book', [
+    { id: 1, title: 'Harry Potter', price: 560, stockAmount: 10, categoryId: 1 },
+    { id: 2, title: 'Game of Thrones', price: 520, stockAmount: 6, categoryId: 1 },
+    { id: 3, title: 'Node.js', price: 300, stockAmount: 4, categoryId: 2 },
+  ])
 }
 
 export default loadFixtures
