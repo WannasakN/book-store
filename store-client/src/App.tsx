6290ba8a16b4d2c20fdab63b5ab610a93fcd86f8
วรรณศักดิ์ หนูปลอด <you007543@gmail.com>
2022-12-17 22:59:11 +0700

    ใช้ compnent BookDetail ในการแสดงรายละเอียดหนังสือ จากเมธอด getAll ที่ mock ไว้

--- store-client/src/App.tsx
+++ store-client/src/App.tsx
@@ -1,9 +1,14 @@
 import Category from "./model/category";
 import repo from "./repo"
+import BookDetail from "./components/bookdetail";
 import { useEffect, useState } from 'react'
 function App() {
   const [categoryList, setCategoryList] = useState<Category[]>([])
-
+  const Book = {
+    id: 1, title: 'Harry Potter', price: 560 , stockAmount: 1,
+    category:{id: 1 , title: 'Fantasy'}
+  }
+  
   const fetchCategoryList = async () => {
     const result = await repo.categories.getAll()
     if (result) {
@@ -20,9 +25,9 @@ function App() {
     {categoryList.map(category => <p key=
     {category.id}> ID : {category.id} Title : 
     {category.title}</p>)}
+    <BookDetail {...Book} key={Book.id}/>
   </div>
-
-  )
+  ) 
 }
 
 export default App;
