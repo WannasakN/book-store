884ac25d72564956fbccf135a1c6fc49f8ccca86
วรรณศักดิ์ หนูปลอด <you007543@gmail.com>
2022-12-18 02:33:42 +0700

    แก้bookform,app เติมcallBackFn และ submitจนสมบูรณ์

--- store-client/src/App.tsx
+++ store-client/src/App.tsx
@@ -1,8 +1,10 @@
-import Category from "./model/category";
-import repo from "./repo"
-import BookDetail from "./components/bookdetail";
 import { useEffect, useState } from 'react'
-import Book from "./model/book";
+import BookDetail from './components/bookdetail'
+import BookForm from './components/bookForm'
+import Book from './model/book'
+import Category from './model/category'
+import repo from './repo'
+
 function App() {
   const [bookList, setBookList] = useState<Book[]>([])
   const [categoryList, setCategoryList] = useState<Category[]>([])
@@ -36,15 +38,18 @@ function App() {
     await repo.books.delete(id)
     fetchBookList()
   }
+
   useEffect(() => {
     fetchCategoryList()
     fetchBookList()
-
-  },[filter])
-
+  }, [filter])
 
   return (
     <div>
+      <div>
+        <BookForm book={{}} categoryList={categoryList} callbackFn={onCreateBook} />
+        <hr />
+      </div>
       <div>
         <select onChange={e => setFilter(e.target.value)}>
           <option value={''}>All</option>
@@ -55,13 +60,13 @@ function App() {
       {bookList.map(book =>
         <div key={book.id}>
           <BookDetail {...book} />
+          <BookForm book={book} categoryList={categoryList} callbackFn={onUpdateBook} />
+          <button onClick={e => onDeleteBook(book.id)}>Delete</button>
+          <hr />
         </div>
       )}
     </div>
   );
 }
-
-
 export default App;
-
 
